ca7ff8a165ebc7660fe9f33bb5cc87218c36a7f1
Eduardo <ejrb1234@gmail.com>
2024-06-08 11:19:58 -0400

    MUI is here! 🐐

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,10 @@
+import { Button } from '@mui/material'
+
 export default function HomePage() {
   return (
     <div>
       <h1>HomePage</h1>
+      <Button variant="contained">Contained</Button>
     </div>
   )
 }
