7751008b21cd851a62e926ac5598ac2f2ffb07e9
Eduardo <ejrb1234@gmail.com>
2024-06-22 16:46:58 -0400

    feat: GalleSection is done!

--- src/app/components/organisms/sections/GallerySection.jsx
+++ src/app/components/organisms/sections/GallerySection.jsx
@@ -5,7 +5,7 @@ import { Title } from '../../atoms/ui'
 export const GallerySection = () => {
   return (
     <div className={styles.container}>
-      <div style={{ gridColumn: '1/8', gridRow: '1/3' }}>
+      <div className={styles.content}>
         <Title type="secondary">
           {`From Clutter to Clean: Our Junk Removal Services in Action`}
         </Title>
@@ -13,66 +13,39 @@ export const GallerySection = () => {
           {`Whether you're dealing with a small amount of clutter or a large-scale cleanout, we're here to help you achieve a peaceful and organized living space.`}
         </p>
       </div>
-      <Image
-        src="/media-placeholder.jpg"
-        alt="junk-squad"
-        width={1280}
-        height={736}
-        style={{
-          width: '100%',
-          height: '100%',
-          gridColumn: '8/13',
-          gridRow: '1/3',
-        }}
-      />
-      <Image
-        src="/media-placeholder.jpg"
-        alt="junk-squad"
-        width={1280}
-        height={736}
-        style={{
-          width: '100%',
-          height: '100%',
-          gridColumn: '1/4',
-          gridRow: '3/5',
-        }}
-      />
-      <Image
-        src="/media-placeholder.jpg"
-        alt="junk-squad"
-        width={1280}
-        height={736}
-        style={{
-          width: '100%',
-          height: '100%',
-          gridColumn: '4/10',
-          gridRow: '3/5',
-        }}
-      />
-      <Image
-        src="/media-placeholder.jpg"
-        alt="junk-squad"
-        width={1280}
-        height={736}
-        style={{
-          width: '100%',
-          height: '100%',
-          gridColumn: '10/13',
-          gridRow: '3/5',
-        }}
-      />
-      <Image
-        src="/media-placeholder.jpg"
-        alt="junk-squad"
-        width={1280}
-        height={736}
-        style={{
-          width: '100%',
-          height: '100%',
-          gridColumn: '1/4',
-          gridRow: '3/5',
-        }}
-      />
+
+      <div className={styles['image-container']}>
+        <Image
+          src="/media-placeholder.jpg"
+          alt="junk-squad"
+          width={1280}
+          height={736}
+        />
+      </div>
+      <div className={styles['image-container']}>
+        <Image
+          src="/media-placeholder.jpg"
+          alt="junk-squad"
+          width={1280}
+          height={736}
+        />
+      </div>
+      <div className={styles['image-container']}>
+        <Image
+          src="/media-placeholder.jpg"
+          alt="junk-squad"
+          width={1280}
+          height={736}
+        />
+      </div>
+      <div className={styles['image-container']}>
+        <Image
+          src="/media-placeholder.jpg"
+          alt="junk-squad"
+          width={1280}
+          height={736}
+        />
+      </div>
     </div>
   )
 }
