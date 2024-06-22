3a27f6557c9145fc16f4a9b736dc62f665d5b29b
Eduardo <ejrb1234@gmail.com>
2024-06-22 15:35:10 -0400

    fix: no volumen, no email + styles on FormSection

--- src/app/components/organisms/form/Form.jsx
+++ src/app/components/organisms/form/Form.jsx
@@ -2,14 +2,7 @@
 import styles from './Form.module.css'
 import { useFormData } from '@/hooks/useFormData'
 import { DatePicker } from '@/components/molecules/DatePicker'
-import {
-  Box,
-  Button,
-  InputAdornment,
-  MenuItem,
-  Select,
-  TextField,
-} from '@mui/material'
+import { Box, Button, InputAdornment, TextField } from '@mui/material'
 
 export const Form = () => {
   const { formData, handleChange, handleDateChange, handleSubmit, error } =
@@ -18,8 +11,8 @@ export const Form = () => {
   return (
     <Box autoComplete="off" component={'form'} className={styles.form}>
       <h3 className={styles.title}>
-        Fill out the form below to schedule a consultation and take the first
-        step towards a clutter-free life!
+        {`Fill out the form below to schedule a consultation and take the first
+        step towards a clutter-free life!`}
       </h3>
       <TextField
         label="Name"
@@ -30,16 +23,6 @@ export const Form = () => {
         error={error.errorName}
         helperText={error.messageName}
         inputProps={{ maxLength: 48 }}
-      />
-      <TextField
-        label="Email"
-        name="email"
-        type="email"
-        value={formData.email}
-        onChange={handleChange}
-        error={error.errorEmail}
-        helperText={error.messageEmail}
-        inputProps={{ maxLength: 62 }}
       />
       <TextField
         label="Phone number"
@@ -71,24 +54,12 @@ export const Form = () => {
         label="Select your pickup date"
         name="date"
       />
-      <Select
-        value={formData.volumen}
-        label="Volumen"
-        name="volumen"
-        onChange={handleChange}
-      >
-        <MenuItem value={'1/4 Truck'}>1/4 Truck</MenuItem>
-        <MenuItem value={'1/2 Truck'}>1/2 Truck</MenuItem>
-        <MenuItem value={'3/4 Truck'}>3/4 Truck</MenuItem>
-        <MenuItem value={'Full Truck'}>Full truck 🚛</MenuItem>
-        <MenuItem value={'+1 Truck'}>More!</MenuItem>
-      </Select>
 
       <Button
         variant="contained"
         type="submit"
         sx={{ width: '100%' }}
-        // disabled={error.errorName || error.errorEmail || error.errorPhone}
+        // disabled={error.errorName || error.errorPhone}
         onClick={handleSubmit}
       >
         Send
--- src/app/components/organisms/sections/FormSection.jsx
+++ src/app/components/organisms/sections/FormSection.jsx
@@ -1,9 +1,25 @@
+import styles from './FormSection.module.css'
+import { Title } from '@/components/atoms/ui'
 import { Form } from '@/components/organisms/form/Form'
+import Image from 'next/image'
 
 export const FormSection = () => {
   return (
-    <div>
-      <Form />
+    <div className={styles.container}>
+      <Title type="secondary">{`Junk-Free Living Starts Here`}</Title>
+      <div className={styles['form-container']}>
+        <Image
+          src="/media-placeholder.jpg"
+          alt="junk-squad"
+          width={1280}
+          height={736}
+          style={{
+            width: '100%',
+            height: '100%',
+          }}
+        />
+        <Form />
+      </div>
     </div>
   )
 }
