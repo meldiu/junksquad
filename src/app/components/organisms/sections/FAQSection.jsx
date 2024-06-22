8d3dd032319a3728b6c9915ead96535488edae40
Eduardo <ejrb1234@gmail.com>
2024-06-22 15:16:54 -0400

    feat: FAQ Section

--- src/app/components/organisms/sections/FAQSection.jsx
+++ src/app/components/organisms/sections/FAQSection.jsx
@@ -1,32 +1,25 @@
+import styles from './FAQSection.module.css'
 import { CustomizedAccordions } from '@/components/organisms/ui/accordion/Accordion.jsx'
 import { Title } from '@/components/atoms/ui'
 import Image from 'next/image'
 
 export const FAQSection = () => {
   return (
-    <div
-      style={{
-        display: 'grid',
-        gridTemplateColumns: 'repeat(12, 1fr)',
-        columnGap: '4.5rem',
-      }}
-    >
-      <div style={{ gridColumn: '1/8' }}>
+    <div className={styles.container}>
+      <div className={styles.content}>
         <Title type="secondary">{`Frequently Asked Questions`}</Title>
         <CustomizedAccordions />
       </div>
 
-      <Image
-        src="/media-placeholder.jpg"
-        alt="junk-squad"
-        width={1280}
-        height={736}
-        style={{
-          width: '100%',
-          height: '100%',
-          gridColumn: '8/13',
-        }}
-      />
+      <div className={styles['image-container']}>
+        <Image
+          src="/media-placeholder.jpg"
+          alt="junk-squad"
+          className={styles.image}
+          width={1280}
+          height={736}
+        />
+      </div>
     </div>
   )
 }
