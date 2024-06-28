d18c2529a5da1dee7f38a803f49f1ba58968a747
Eduardo <ejrb1234@gmail.com>
2024-06-28 17:29:54 -0400

    feat(app): GallerySection is done!

--- src/app/components/organisms/sections/GallerySection.jsx
+++ src/app/components/organisms/sections/GallerySection.jsx
@@ -5,46 +5,59 @@ import { Title } from '@/components/atoms/ui'
 export const GallerySection = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.content}>
-        <Title type="secondary">
-          {`From Clutter to Clean: Our Junk Removal Services in Action`}
-        </Title>
-        <p className={styles.description}>
-          {`Whether you're dealing with a small amount of clutter or a large-scale cleanout, we're here to help you achieve a peaceful and organized living space.`}
-        </p>
+      <div className={styles['first-section']}>
+        <div className={styles.content}>
+          <Title type="secondary">
+            {`From Clutter to Clean: Our Junk Removal Services in Action`}
+          </Title>
+          <p className={styles.description}>
+            {`Whether you're dealing with a small amount of clutter or a large-scale cleanout, we're here to help you achieve a peaceful and organized living space.`}
+          </p>
+        </div>
+
+        <div className={styles['image-container']}>
+          <Image
+            src="/0-galley-img.webp"
+            alt="junk-squad"
+            width={600}
+            height={300}
+            loading="lazy"
+            className={` ${styles.image} ${styles['image-0']}`}
+          />
+        </div>
       </div>
 
-      <div className={styles['image-container']}>
-        <Image
-          src="/0-galley-img.webp"
-          alt="junk-squad"
-          width={1280}
-          height={736}
-        />
-      </div>
-      <div className={styles['image-container']}>
-        <Image
-          src="/1-galley-img.webp"
-          alt="junk-squad"
-          width={1280}
-          height={736}
-        />
-      </div>
-      <div className={styles['image-container']}>
-        <Image
-          src="/2-galley-img.webp"
-          alt="junk-squad"
-          width={1280}
-          height={736}
-        />
-      </div>
-      <div className={styles['image-container']}>
-        <Image
-          src="/3-galley-img.webp"
-          alt="junk-squad"
-          width={1280}
-          height={736}
-        />
+      <div className={styles['second-section']}>
+        <div className={styles['image-container']}>
+          <Image
+            src="/1-galley-img.webp"
+            alt="junk-squad"
+            width={420}
+            height={360}
+            loading="lazy"
+            className={` ${styles.image} ${styles['image-1']}`}
+          />
+        </div>
+        <div className={styles['image-container']}>
+          <Image
+            src="/2-galley-img.webp"
+            alt="junk-squad"
+            width={420}
+            height={360}
+            loading="lazy"
+            className={` ${styles.image} ${styles['image-2']}`}
+          />
+        </div>
+        <div className={styles['image-container']}>
+          <Image
+            src="/3-galley-img.webp"
+            alt="junk-squad"
+            width={420}
+            height={360}
+            loading="lazy"
+            className={` ${styles.image} ${styles['image-3']}`}
+          />
+        </div>
       </div>
     </div>
   )
