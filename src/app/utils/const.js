58d65cc8d1fc0fc51c3736b780e50012aa98e7bb
Eduardo <ejrb1234@gmail.com>
2024-06-28 15:26:30 -0400

    feat(app): FAQ questions-answers completed

--- src/app/utils/const.js
+++ src/app/utils/const.js
@@ -1,27 +1,22 @@
 export const FAQ = [
   {
-    question: 'What types of junk do you remove?',
-    answer:
-      'We offer comprehensive junk removal services to handle a wide range of items, from small household appliances and furniture to large bulky items and even construction debris; in addition, we provide light demolition services for tasks like tearing down sheds, dismantling decks, or removing unwanted structures, ensuring a thorough clean-up.',
+    question: `What types of junk do you remove?`,
+    answer: `We offer comprehensive junk removal services to handle a wide range of items, from small household appliances and furniture to large bulky items and even construction debris; in addition, we provide light demolition services for tasks like tearing down sheds, dismantling decks, or removing unwanted structures, ensuring a thorough clean-up.`,
   },
   {
-    question: 'Do you donate items?',
-    answer:
-      'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna.',
+    question: `Do you donate items?`,
+    answer: `Yes, we are committed to donating as many items as possible to local charities and organizations. We believe in giving your unwanted items a second life whenever feasible, reducing waste and supporting our community.`,
   },
   {
-    question: 'How do you determine pricing?',
-    answer:
-      'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna.',
+    question: `How do you determine pricing?`,
+    answer: `Our pricing is based on the amount of space your items occupy in our trucks. We provide upfront pricing, so you know exactly how much you'll pay before we begin the removal process. Our pricing is competitive and transparent, ensuring you get the best value for your money.`,
   },
   {
-    question: 'Are your workers fully insured and background-checked?',
-    answer:
-      'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna.',
+    question: `Are your workers fully insured and background-checked?`,
+    answer: `Absolutely. Our team members are fully insured and background-checked for your safety and peace of mind. We take the security and well-being of our customers and their properties very seriously.`,
   },
   {
     question: 'What is your process for handling hazardous materials?',
-    answer:
-      'Lorem ipsum dolor sit amet consectetur adipiscing elit Ut et massa mi. Aliquam in hendrerit urna.',
+    answer: `We have a specialized process for handling hazardous materials, such as electronics, chemicals, and batteries. Our team is trained to handle these items safely and responsibly, ensuring they are disposed of in accordance with local regulations and environmental standards.`,
   },
 ]
