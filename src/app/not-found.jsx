fb90f79e2de4a2e826f2b235df0abfb52859ea4f
Eduardo <ejrb1234@gmail.com>
2024-07-07 20:36:17 -0400

    feat(app): #404Page is here!

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,7 +1,31 @@
+import styles from './NotFound.module.css'
+import Image from 'next/image'
+import { Section } from '@/components/atoms/ui'
+import { BUSINESS_PHONE } from '@/utils/const'
+
 export default function NotFoundPage() {
   return (
-    <>
-      <h2>{`#404`}</h2>
-    </>
+    <Section
+      style={{
+        marginTop: '80px',
+        minHeight: 'calc(100dvh - 80px)',
+        position: 'relative',
+      }}
+    >
+      <div className={styles.container}>
+        <div className={styles['image-container']}>
+          <Image src={'/truck-404.svg'} alt="" width={920} height={920} />
+        </div>
+        <div className={styles.content}>
+          <h2 className={styles.title}>{`Oops! That page doesn't exist.`}</h2>
+          <p
+            className={styles.paragraph}
+          >{`We seem to have misplaced the page you were looking for. Don't worry though, we'll help you find what you need!`}</p>
+          <span
+            className={styles['helper-text']}
+          >{`Contact us at ${BUSINESS_PHONE} and we'll point you in the right direction.`}</span>
+        </div>
+      </div>
+    </Section>
   )
 }
